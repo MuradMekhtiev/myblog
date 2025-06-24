a0e69bf7a4718a25b9e5c7b2fe897e309b2b66b1
MuradMekhtiev <60578233+MuradMekhtiev@users.noreply.github.com>
2025-06-24 22:56:31 +0400

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.models import Image
+from wagtail.images.blocks import ImageChooserBlock
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+from django.utils.text import slugify
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=80, unique=True, blank=True)
+
+    panels = [FieldPanel("name")]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class BlogPage(Page):
+    parent_page_types = ['home.BlogIndexPage']
+    subpage_types = []
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250, blank=True)
+    body = RichTextField(blank=True)
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    category = models.ForeignKey(
+        'home.Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='blog_posts'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        FieldPanel('main_image'),
+        FieldPanel('category'),
+        FieldPanel('tags'),
+    ]
+
+
+class BlogIndexPage(Page):
+    subpage_types = ['home.BlogPage']
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['posts'] = BlogPage.objects.live().descendant_of(self).order_by('-date')
+        return context
---
+++ myblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h*v)#wmj71%3sug$8((q2@5)&i!)p@7@d==iz(+yco*_*aax&-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
